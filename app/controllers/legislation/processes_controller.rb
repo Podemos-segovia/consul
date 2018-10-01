consuldemocracy/consuldemocracy

Create @proposal variable con process controller for proposals list that use random seed

app/controllers/legislation/processes_controller.rb
@@ -88,9 +88,9 @@ class Legislation::ProcessesController < Legislation::BaseController
   def proposals
     set_process
     @phase = :proposals_phase
-
+    @proposals = Legislation::Proposal.where(process: @process)
     if @process.proposals_phase.started? || current_user.administrator?
-      set_legislation_proposal_votes(@process.proposals)
+      set_legislation_proposal_votes(@proposals)
       render :proposals
     else
       render :phase_not_open

Podemos-segovia/consul

app/controllers/legislation/processes_controller.rb
@@ -89,8 +89,9 @@ class Legislation::ProcessesController < Legislation::BaseController
     set_process
     @phase = :proposals_phase
 
+    @proposals = Legislation::Proposal.where(process: @process)
     if @process.proposals_phase.started?
-      legislation_proposal_votes(@process.proposals)
+      legislation_proposal_votes(@proposals)
       render :proposals
     else
       render :phase_not_open